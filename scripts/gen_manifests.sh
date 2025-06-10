b7920db0e995a25205383dfa3b692b7569ad436c
Moti Asayag <masayag@redhat.com>
2025-06-10 15:17:26 +0300

    Updating repository to OSL-1.36
    Within this PR, we prepare the tools for using OSL 1.36:
    * Update builder image to use a custom builder image based on 1.36 with precached dependencies
    * Update pipeline image to use kn-workflow 1.36 that introduced some goodies that led to:
    * Update scripts that used to hack lack of support in kn-workflow (specify image, profile and generate secrets)
    * Update persistence property to its new name
    
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- scripts/gen_manifests.sh
+++ scripts/gen_manifests.sh
@@ -18,11 +18,11 @@ command -v kubectl
 
 cd "${WORKFLOW_FOLDER}"
 
-echo -e "\nquarkus.flyway.migrate-at-start=true" >> application.properties
+echo -e "\nkie.flyway.enabled=true" >> application.properties
 
-# TODO Update to use --skip-namespace when the following is released
-# https://github.com/apache/incubator-kie-tools/pull/2136
-kn-workflow gen-manifest --namespace ""
+kn-workflow gen-manifest --profile='gitops' \
+        --image="${WORKFLOW_IMAGE_REGISTRY}/${WORKFLOW_IMAGE_NAMESPACE}/${WORKFLOW_IMAGE_REPO}:${WORKFLOW_IMAGE_TAG}" \
+        --namespace=""
 
 # Enable bash's extended blobing for better pattern matching
 shopt -s extglob
@@ -49,15 +49,9 @@ fi
 # the main sonataflow file will have a prefix of variable number, 01 or 02 and so on, because manifests created by
 # gen-manifests are now sorted by name. We need to take *-sonataflow-$workflow_id.yaml to resolve that.
 SONATAFLOW_CR=$(printf '%s' manifests/*-sonataflow_"${workflow_id}".yaml)
-yq --inplace eval '.metadata.annotations["sonataflow.org/profile"] = "gitops"' "${SONATAFLOW_CR}"
 
-yq --inplace ".spec.podTemplate.container.image=\"${WORKFLOW_IMAGE_REGISTRY}/${WORKFLOW_IMAGE_NAMESPACE}/${WORKFLOW_IMAGE_REPO}:${WORKFLOW_IMAGE_TAG}\"" "${SONATAFLOW_CR}"
-
-if test -f "secret.properties"; then
-  yq --inplace ".spec.podTemplate.container.envFrom=[{\"secretRef\": { \"name\": \"${workflow_id}-creds\"}}]" "${SONATAFLOW_CR}"
-  kubectl create secret generic "${workflow_id}-creds" --from-env-file=secret.properties --dry-run=client -oyaml > "manifests/00-secret_${workflow_id}.yaml"
-fi
-
+# The following properties are set in the Sonataflow CR, for each workflow to enable persistence.
+# TODO: It should be replaced with a single definition in the SonataflowPlatform CR
 if [ "${ENABLE_PERSISTENCE}" = true ]; then
     yq --inplace ".spec |= (
       . + {
